c1d69d51185f299fd764ead002e3be13f8f5b131
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-08-26 22:56:49 +0200

    I modified the view to return a JsonResponse.

--- properties/views.py
+++ properties/views.py
@@ -1,13 +1,11 @@
 from django.views.decorators.cache import cache_page
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.http import JsonResponse
 from .models import Property
 from .serializers import PropertySerializer
 
 # Cache for 15 minutes (60*15 seconds)
 @cache_page(60 * 15)
-@api_view(['GET'])
 def property_list(request):
     properties = Property.objects.all()
     serializer = PropertySerializer(properties, many=True)
-    return Response(serializer.data)
+    return JsonResponse(serializer.data, safe=False)
