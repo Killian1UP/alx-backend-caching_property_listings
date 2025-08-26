7aa9109fa9dd7409a3ee9476df4943df4636dfe0
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-08-26 22:52:52 +0200

    Added an api view for property list.

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Property
+from .serializers import PropertySerializer
 
-# Create your views here.
+# Cache for 15 minutes (60*15 seconds)
+@cache_page(60 * 15)
+@api_view(['GET'])
+def property_list(request):
+    properties = Property.objects.all()
+    serializer = PropertySerializer(properties, many=True)
+    return Response(serializer.data)
