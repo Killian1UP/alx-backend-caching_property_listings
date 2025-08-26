205c15209dfaf5683413537ec8172f3cdad7ef58
Ikaelelo Motlhako <ikaelelo.motlhako@gmail.com>
2025-08-26 23:09:32 +0200

    Updated the property_list to use the function from utils.py

--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,8 @@
-from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
-from .models import Property
 from .serializers import PropertySerializer
+from .utils import get_all_properties
 
-# Cache for 15 minutes (60*15 seconds)
-@cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.all()
+    properties = get_all_properties()
     serializer = PropertySerializer(properties, many=True)
     return JsonResponse({"properties": serializer.data})
